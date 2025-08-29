9ba168b8f8dab2cc028fd3cc93fa08fcb42838b9
Ioannis Tsiros <itsiros@2-C-7.42heilbronn.de>
2025-08-29 19:40:10 +0200

    progress

--- ex02/PmergeMe.cpp
+++ ex02/PmergeMe.cpp
@@ -1,7 +1,10 @@
 #include "./PmergeMe.hpp"
+#include <algorithm>
+#include <cmath>
 #include <iostream>
 #include <string>
 #include <utility>
+#include <vector>
 
 template <typename T>
 static void printDebugs(T container, const std::string debug) {
@@ -12,6 +15,10 @@ static void printDebugs(T container, const std::string debug) {
     std::cout << *it << " ";
   }
   std::cout << std::endl;
+}
+
+static int jacobNumber(int n) {
+  return std::round((std::pow(2, n + 1) + std::pow(-1, n)) / 3);
 }
 
 template <typename T> static void jacob(T container, unsigned long n) {
@@ -29,9 +36,72 @@ template <typename T> static void jacob(T container, unsigned long n) {
     }
     ++i;
   }
+
   n = n << 1;
+
   printDebugs(container, "groups of " + std::to_string(n / 2));
   jacob(container, n);
+
+  if (n > container.size()) {
+    return;
+  }
+
+  n >>= 1;
+  if (n * 3 > container.size()) {
+    n >>= 1;
+  }
+
+  int jacobIndex = 2;
+  int Jnum = jacobNumber(jacobIndex);
+  std::cout << "\nJacob number is: " << Jnum << std::endl;
+
+  std::vector<unsigned long> main;
+  std::vector<unsigned long> pending;
+
+  std::cout << "\nGroup size is: " << n << std::endl;
+
+  bool stop = false;
+  unsigned long j = 0;
+
+  while (j < n * 2) {
+    main.push_back(container[j++]);
+  }
+
+  while (j < container.size() - (container.size() % n)) {
+    for (size_t k = 0; k < n && j < container.size(); ++k, ++j) {
+      if (stop) {
+        main.push_back(container[j]);
+      } else {
+        pending.push_back(container[j]);
+      }
+    }
+    stop = !stop;
+  }
+
+  int numBGroups = (pending.size() / n);
+  std::cout << "\nSize / n is: " << numBGroups << std::endl;
+
+  while (Jnum <= numBGroups + 1) {
+    for (auto it = main.begin() + n; it != main.end(); it += n) {
+      if (pending[Jnum * n] < *it) {
+        // append
+      }
+    }
+    Jnum = jacobNumber(++jacobIndex);
+  }
+
+  // i = 0;
+  // for (auto it = container.begin(); it != container.end(); ++it) {
+  //   if (i % n == n - 1 && *(it - n / 2) > *it) {
+  //     for (unsigned long temp = i; temp > i - n / 2; --temp) {
+  //       std::swap(container[temp], container[temp - n / 2]);
+  //     }
+  //   }
+  //   ++i;
+  // }
+
+  printDebugs(main, "Main chain");
+  printDebugs(pending, "Pending chain");
 }
 
 template <>
