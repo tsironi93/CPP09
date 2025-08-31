b624ecb1fc3d44fd9236f1e9b3ce98b32cf244aa
Ioannis Tsiros <itsiros@2-C-7.42heilbronn.de>
2025-08-31 17:48:16 +0200

    almostThere

--- ex02/PmergeMe.cpp
+++ ex02/PmergeMe.cpp
@@ -10,7 +10,7 @@
 template <typename T>
 static void printDebugs(T container, const std::string debug) {
 
-  std::cout << "\n\n========================" << debug
+  std::cout << "========================" << debug
             << "========================" << std::endl;
   for (auto it = container.begin(); it != container.end(); ++it) {
     std::cout << *it << " ";
@@ -22,7 +22,7 @@ static int jacobNumber(int n) {
   return std::round((std::pow(2, n + 1) + std::pow(-1, n)) / 3);
 }
 
-template <typename T> static void jacob(T container, unsigned long n) {
+template <typename T> static void jacob(T &container, unsigned long n) {
 
   if (n > container.size()) {
     return;
@@ -30,7 +30,7 @@ template <typename T> static void jacob(T container, unsigned long n) {
 
   int i = 0;
   for (auto it = container.begin(); it != container.end(); ++it) {
-    if (i % n == n - 1 && *(it - n / 2) > *it) {
+    if (i >= static_cast<int>(n / 2) && i % n == n - 1 && *(it - n / 2) > *it) {
       for (unsigned long temp = i; temp > i - n / 2; --temp) {
         std::swap(container[temp], container[temp - n / 2]);
       }
@@ -47,19 +47,17 @@ template <typename T> static void jacob(T container, unsigned long n) {
     return;
   }
 
-  n >>= 1;
-  if (n * 3 > container.size()) {
-    n >>= 1;
-  }
+  n = n >> 1;
+  n = n >> 1;
 
   int jacobIndex = 2;
   int Jnum = jacobNumber(jacobIndex);
-  std::cout << "\nJacob number is: " << Jnum << std::endl;
+  // std::cout << "\nJacob number is: " << Jnum << std::endl;
 
   std::vector<unsigned long> main;
   std::vector<unsigned long> pending;
 
-  std::cout << "\nGroup size is: " << n << std::endl;
+  // std::cout << "\nGroup size is: " << n << std::endl;
 
   bool stop = false;
   unsigned long j = 0;
@@ -79,62 +77,101 @@ template <typename T> static void jacob(T container, unsigned long n) {
     stop = !stop;
   }
 
-  int numBGroups = (pending.size() / n);
-  std::cout << "\nSize / n is: " << numBGroups << std::endl;
+  // int numBGroups = (pending.size() / n);
+  // std::cout << "\nSize / n is: " << numBGroups << std::endl;
 
   printDebugs(main, "Main chain");
   printDebugs(pending, "Pending chain");
 
   static size_t numComparisons;
   int counter = 1;
+  size_t startingBounds = n - 1;
+  size_t endingBounds = main.size() - 1;
+  size_t insert_index;
   while (pending.size() != 0) {
 
-    size_t startingBounds = 2 * n - 1;
-    size_t endingBounds = pending.size() - 1;
+    size_t jprev = Jnum;
+    long int temp = (Jnum - counter) * n - 1;
+    // std::cout << "temp is = " << temp << std::endl;
+    if (temp < 0) {
+      counter = 0 + jprev;
+      Jnum = jacobNumber(++jacobIndex);
+      insert_index = (Jnum - counter) * n - 1;
+      endingBounds = main.size() - 1;
+
+      while (insert_index >= pending.size()) {
+        counter++;
+        insert_index = (Jnum - counter) * n - 1;
+      }
+
+    } else {
+      insert_index = temp;
+    }
+
+    // std::cout << "Temp after = " << insert_index << std::endl;
 
     for (size_t k = startingBounds; k <= main.size(); k += n) {
 
-      size_t insert_index = (Jnum - counter) * n - 1;
-
-      if (insert_index > pending.size() || pending.size() == 0) {
+      if (insert_index >= pending.size() || pending.size() == 0) {
         break;
       }
 
       std::cout << "pending < main : " << pending[insert_index] << " < "
                 << main[k] << std::endl;
 
-      if (main[k] > main[endingBounds]) {
-        main.insert(main.begin() + k + 1,
-                    pending.begin() + insert_index - n + 1,
-                    pending.begin() + insert_index + 1);
-        pending.erase(pending.begin() + insert_index - n + 1,
+      numComparisons++;
+      if (pending[insert_index] < main[k]) {
+        if (n != 1) {
+          main.insert(main.begin() + k - n + 1,
+                      pending.begin() + insert_index - n + 1,
                       pending.begin() + insert_index + 1);
-        printDebugs(main, "Insersion");
-        printDebugs(pending, "Erasing");
-        endingBounds = k + 1;
-      } else if (pending[insert_index] < main[k]) {
-        numComparisons++;
-        main.insert(main.begin() + k - n + 1,
-                    pending.begin() + insert_index - n + 1,
-                    pending.begin() + insert_index + 1);
-        pending.erase(pending.begin() + insert_index - n + 1,
+          pending.erase(pending.begin() + insert_index - n + 1,
+                        pending.begin() + insert_index + 1);
+          endingBounds = k - n;
+        } else {
+          main.insert(main.begin() + k - n + 1, pending[insert_index]);
+          pending.erase(pending.begin() + insert_index);
+        }
+        printDebugs(main, "Insersion after Comparisons");
+        printDebugs(pending, "Erasing after Comparisons");
+        break;
+      } else if (main[k] == main[endingBounds] &&
+                 endingBounds != main.size() - 1) {
+        // std::cout << "=======No Comparisons Zone===========" << std::endl;
+        if (n != 1) {
+          main.insert(main.begin() + k + 1,
+                      pending.begin() + insert_index - n + 1,
                       pending.begin() + insert_index + 1);
-        printDebugs(main, "Insersion");
-        printDebugs(pending, "Erasing");
-        endingBounds = k + 1;
+          pending.erase(pending.begin() + insert_index - n + 1,
+                        pending.begin() + insert_index + 1);
+          endingBounds = k;
+        } else {
+          main.insert(main.begin() + k + 1, main[k]);
+          pending.erase(pending.begin() + k);
+        }
+        printDebugs(main, "Auto Insersion");
+        printDebugs(pending, "Auto Erasing");
+        break;
       }
+
+      std::cout << "Ending Bounds Number: " << main[endingBounds] << std::endl;
     }
     counter++;
   }
 
   int nonPartisipating = container.size() % n;
-  while (nonPartisipating--) {
-    main.push_back(container[container.size() - nonPartisipating]);
-    container.clear();
-    std::copy(main.begin(), main.end(), container.begin());
+  if (n != 1 || nonPartisipating != 0) {
+    while (nonPartisipating--) {
+      main.push_back(container[container.size() - nonPartisipating - 1]);
+    }
   }
-  std::cout << "malaaaaaaaaaaaaaaaaaaaaaaaaakaaa\n"
-            << "Number of Comparisons is : " << numComparisons << std::endl;
+  container.clear();
+  container.insert(container.end(), main.begin(), main.end());
+  main.clear();
+  pending.clear();
+  // std::cout << "malaaaaaaaaaaaaaaaaaaaaaaaaakaaa\n"
+  //           << "Number of Comparisons is : " << numComparisons << std::endl;
+  printDebugs(container, "After");
 }
 
 template <>
